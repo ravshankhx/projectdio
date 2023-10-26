c3bbc1b1d3f714f9af4810e1299a18351ac6e175
ravshankhx <r.xasanov@uic.group>
2023-10-27 00:14:20 +0500

    feat:statuscode

--- lib/features/data/repositories/repository_impl.dart
+++ lib/features/data/repositories/repository_impl.dart
@@ -7,7 +7,7 @@ class Repository {
   Future<List<DataModel>> getData() async {
     final response = await dio.get("https://cat-fact.herokuapp.com/facts");
 
-    if (response.statusCode != null) {
+    if (response.statusCode != null && response.statusCode! >= 200 && response.statusCode! < 300) {
       return (response.data as List<dynamic>).map((e) => DataModel.fromJson(e)).toList();
     } else {
       throw Exception("Error");
